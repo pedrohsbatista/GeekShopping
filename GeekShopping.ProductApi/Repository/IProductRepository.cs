c4e25631374141240269039b95aaac91b9d16bd0
Pedro Batista <pedrohenrique.sb07@gmail.com>
2022-04-09 12:01:07 -0300

    Interface IProductRepository

--- GeekShopping.ProductApi/Repository/IProductRepository.cs
+++ GeekShopping.ProductApi/Repository/IProductRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace GeekShopping.ProductApi.Repository
+﻿using GeekShopping.ProductApi.Data.ValueObjects;
+
+namespace GeekShopping.ProductApi.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductVO>> FindAll();
+        Task<ProductVO> FindById(long id);
+        Task<ProductVO> Create(ProductVO productVO);
+        Task<ProductVO> Update(ProductVO productVO);
+        Task<bool> Delete(long id);
     }
 }
