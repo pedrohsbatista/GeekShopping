da32a16ed125ddc58576acc8d0eb6027bfe6db75
Pedro Batista <pedrohenrique.sb07@gmail.com>
2022-04-09 18:10:38 -0300

    Controller Product

---
+++ GeekShopping.ProductApi/Controllers/ProductController.cs
@@ -0,0 +1,67 @@
+﻿using GeekShopping.ProductApi.Data.ValueObjects;
+using GeekShopping.ProductApi.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentException(nameof(repository));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
+        {
+            var products = await _repository.FindAll();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductVO>> FindById(long id)
+        {
+            var product = await _repository.FindById(id);
+
+            if (product == null)
+                return NotFound();
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductVO>> Create(ProductVO productVO)
+        {
+            if (productVO == null)
+                return BadRequest();
+
+            var result = await _repository.Create(productVO);
+            return Ok(result);  
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ProductVO>> Update(ProductVO productVO)
+        {
+            if (productVO == null)
+                return BadRequest();
+
+            var result = await _repository.Update(productVO);
+            return Ok(result);      
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(long id)
+        {
+            var ok = await _repository.Delete(id);
+
+            if (!ok)
+                return BadRequest();
+
+            return Ok(ok);
+        }
+    }
+}
