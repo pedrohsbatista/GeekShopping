07dd9d3121579ec88d72eb1240ef4150ae8d5d6f
Pedro Batista <pedrohenrique.sb07@gmail.com>
2022-04-09 18:25:13 -0300

    Seed Product

---
+++ GeekShopping.ProductApi/Migrations/20220409211957_SeedProductDataTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.ProductApi.Migrations
+{
+    public partial class SeedProductDataTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[] { 1L, "Categoria A", "Descrição A", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png", "Produto A", 10.1m });
+
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[] { 2L, "Categoria B", "Descrição B", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png", "Produto B", 20.2m });
+
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[] { 3L, "Categoria C", "Descrição C", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png", "Produto C", 30.3m });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 3L);
+        }
+    }
+}
--- GeekShopping.ProductApi/Model/Context/MySqlContext.cs
+++ GeekShopping.ProductApi/Model/Context/MySqlContext.cs
@@ -9,9 +9,40 @@ namespace GeekShopping.ProductApi.Model.Context
         }
 
         public MySqlContext(DbContextOptions<MySqlContext> options) : base(options)
-        {                
+        {
         }
 
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 1,
+                Name = "Produto A",
+                Price = new decimal(10.10),
+                Description = "Descrição A",
+                CategoryName = "Categoria A",
+                ImageUrl = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 2,
+                Name = "Produto B",
+                Price = new decimal(20.20),
+                Description = "Descrição B",
+                CategoryName = "Categoria B",
+                ImageUrl = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 3,
+                Name = "Produto C",
+                Price = new decimal(30.30),
+                Description = "Descrição C",
+                CategoryName = "Categoria C",
+                ImageUrl = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png"
+            });
+        }
     }
 }
