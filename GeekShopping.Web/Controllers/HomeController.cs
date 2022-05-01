338209878f3269ffb8c6831ed18625603b02c626
Pedro Batista <pedrohenrique.sb07@gmail.com>
2022-05-01 11:36:49 -0300

    Chamada para exibir  a página de detalhe do produto

--- GeekShopping.Web/Controllers/HomeController.cs
+++ GeekShopping.Web/Controllers/HomeController.cs
@@ -20,9 +20,17 @@ namespace GeekShopping.Web.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var token = await HttpContext.GetTokenAsync("access_token");
+            var token = await HttpContext.GetTokenAsync("");
             var products = await _productService.FindAllProducts(token);
             return View(products);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Details(int id)
+        {
+            var token = await HttpContext.GetTokenAsync("access_token");
+            var product = await _productService.FindProductById(id, token);
+            return View(product);
         }
 
         public IActionResult Privacy()
