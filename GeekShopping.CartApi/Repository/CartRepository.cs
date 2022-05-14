ba9b52b6d15630edd076b330e61373146be10ae4
Pedro Batista <pedrohenrique.sb07@gmail.com>
2022-05-14 18:37:52 -0300

    Repository CartApi

---
+++ GeekShopping.CartApi/Repository/CartRepository.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using GeekShopping.CartApi.Data.ValueObjects;
+using GeekShopping.CartApi.Model.Context;
+
+namespace GeekShopping.CartApi.Repository
+{
+    public class CartRepository : ICartRepository
+    {
+        private readonly MySqlContext _mySqlContext;
+        private IMapper _mapper;
+
+        public CartRepository(MySqlContext mySqlContext, IMapper mapper)
+        {
+            _mySqlContext = mySqlContext;
+            _mapper = mapper;
+        }
+
+        public Task<bool> ApplyCoupon(string userId, long couponCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ClearCart(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartVO> FindCartByUserId(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveCoupon(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveFromCart(long cartDetailsId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartVO> SaveOrUpdateCart(CartVO cartVO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GeekShopping.CartApi/Repository/ICartRepository.cs
@@ -0,0 +1,19 @@
+﻿using GeekShopping.CartApi.Data.ValueObjects;
+
+namespace GeekShopping.CartApi.Repository
+{
+    public interface ICartRepository
+    {
+        Task<CartVO> FindCartByUserId(string userId);
+
+        Task<CartVO> SaveOrUpdateCart(CartVO cartVO);
+
+        Task<bool> RemoveFromCart(long cartDetailsId);
+
+        Task<bool> ApplyCoupon(string userId, long couponCode);
+
+        Task<bool> RemoveCoupon(string userId);
+
+        Task<bool> ClearCart(string userId);
+    }
+}
