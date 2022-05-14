6f92d9703d59786bee172383c8991c3e603aed2b
Pedro Batista <pedrohenrique.sb07@gmail.com>
2022-05-14 13:59:36 -0300

    Model CartApi

--- GeekShopping.CartApi/Model/Product.cs
+++ GeekShopping.CartApi/Model/Product.cs
@@ -1,12 +1,15 @@
-﻿using GeekShopping.CartApi.Model.Base;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GeekShopping.CartApi.Model
 {
     [Table("product")]
-    public class Product : BaseEntity
+    public class Product
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Column("id")]
+        public long Id { get; set; }
+
         [Column("name")]
         [Required]
         [StringLength(150)]
